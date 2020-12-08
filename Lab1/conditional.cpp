dae9162dd313c2afd5df904e075213cdc5251317
sujal11 <55621019+sujal11@users.noreply.github.com>
2020-12-08 16:09:29 +0530

    lab1 ques 1

---
+++ Lab1/conditional.cpp
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<iostream.h>
+void main()
+{
+int a,b,c;
+clrscr();
+cout<<"enter two numbers\n";
+cin>>a>>b;
+c=a>b?a:b;
+cout<<"largest of your two numbers is\n"<<c;
+getch();
+
+}
